c1df0156c11beec46bf39a1c27e46e1d4e389ec1
Your Name <psoyeon0127@naver.com>
2023-03-20 12:15:17 +0900

    [EDIT] vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -5,9 +5,17 @@ class Vote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='ping')
-    async def ping(self, ctx):
-        await ctx.send("pong!")
+    @commands.command(name='vote')
+    async def vote(self, ctx, *args):
+        question = ' '.join(args[:-1])
+        answers = options[-1]
+
+        embed = discord.Embed(title = question, description=answers)
+
+        message = await ctx.send(embed = embed)
+
+        for arg in range(len(options)-1):
+            await message.add_reaction(chr(127462 + i))
 
 async def setup(bot):
     await bot.add_cog(Vote(bot))
