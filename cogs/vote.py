b6dcc984f9fb42d8a5abe836b898185b8cff27fc
Your Name <psoyeon0127@naver.com>
2023-03-23 18:01:08 +0900

    [EDIT] add options with emoji

--- cogs/vote.py
+++ cogs/vote.py
@@ -7,15 +7,23 @@ class Vote(commands.Cog):
 
     @commands.command(name='vote')
     async def vote(self, ctx, *options):
-        question = ' '.join(options[:-1])
-        answers = options[-1]
+        question = options[0]
+        answers = ' '.join(options[1:])
 
         embed = discord.Embed(title = question, description=answers, color = discord.Color.green())
 
         message = await ctx.send(embed = embed)
 
-        for arg in range(len(options)-1):
+        for i in range(len(options)-1):
             await message.add_reaction(chr(127462 + i))
+
+        reactions = await message.await_reactions(timeout=1)
+
+        result = []
+
+        for i, reaction in enumerate(reactions):
+            result.append(f'{options[i]}: {reaction.count -1}표')
+        await ctx.send('\n'.join(result))
 
 async def setup(bot):
     await bot.add_cog(Vote(bot))
